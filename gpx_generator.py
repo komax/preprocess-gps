097e3e8a16acbe8b4e8ce70a49a1af9e29598d5e
Maximilian Konzack <mkonzack@acm.org>
2013-11-06 11:20:58 +0100

    Handling csv file or directory correctly

--- gpx_generator.py
+++ gpx_generator.py
@@ -1,24 +1,29 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import os
 import sys
+import glob
 
-args = sys.argv[1:]
-
-def generate_gpx():
-    assert len(args) >= 1
-    csv_file = args[0]
+def generate_gpx(csv_file):
     (file_name, file_ext) = os.path.splitext(csv_file)
     assert file_ext == '.csv'
     assert csv_file.endswith('.csv')
 
     command_string = "gpsbabel -i iblue747 -f %s.csv -o gpx -F %s.gpx" %\
       (file_name, file_name)
-    print command_string
+    print(command_string)
     os.system(command_string)
 
 def main():
-    generate_gpx()
+    args = sys.argv[1:]
+    assert len(args) >= 1
+    arg = args[0]
+    if os.path.isfile(arg):
+        generate_gpx(arg)
+    elif os.path.isdir(arg):
+        for csv_file in glob.glob(arg+'/*.csv'):
+            generate_gpx(csv_file)
+    
 
 if __name__ == '__main__':
     main()
