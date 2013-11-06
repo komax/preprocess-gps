e7263a3d3fe4fe5e54fd2a11b22c7a65c0cd87f6
Maximilian Konzack <mkonzack@acm.org>
2013-11-06 14:47:12 +0100

    Merging of *.gpx into one gpx file

---
+++ gpx_merger.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+import glob
+
+def merge_gpx(directory_name, out_file):
+    parameter_str = ''
+    for gpx_file in glob.glob(directory_name+'*.gpx'):
+        parameter_str += ' -f %s' % (gpx_file)
+
+    if not parameter_str:
+        return
+
+    command_string = "gpsbabel -i gpx %s -o gpx -F %s" %\
+      (parameter_str, out_file)
+    print(command_string)
+    os.system(command_string)
+
+def main():
+    args = sys.argv[1:]
+    assert len(args) >= 1
+    (dir_name, out_file) = args[0:2]
+    if os.path.isdir(dir_name):
+        merge_gpx(dir_name, out_file)
+
+if __name__ == '__main__':
+    main()
+
