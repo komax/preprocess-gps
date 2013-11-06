cd0a28e1c75eb0e23a8f3e0b719f2f5a830e3536
Maximilian Konzack <mkonzack@acm.org>
2013-11-06 10:47:21 +0100

    simple converter for one csv file

--- gpx_generator.py
+++ gpx_generator.py
@@ -8,13 +8,17 @@ args = sys.argv[1:]
 def generate_gpx():
     assert len(args) >= 1
     csv_file = args[0]
+    (file_name, file_ext) = os.path.splitext(csv_file)
+    assert file_ext == '.csv'
     assert csv_file.endswith('.csv')
 
-    command_string = 'gpsbabel -i iblue747 -f 003-239.csv -o gpx -F 003-239.gpx'
+    command_string = "gpsbabel -i iblue747 -f %s.csv -o gpx -F %s.gpx" %\
+      (file_name, file_name)
+    print command_string
     os.system(command_string)
 
 def main():
-    pass
+    generate_gpx()
 
 if __name__ == '__main__':
     main()
