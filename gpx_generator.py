c593a28be90f35204c0e3b247249d545a79a6920
Maximilian Konzack <mkonzack@acm.org>
2014-01-16 16:38:08 +0100

    Code styling

--- gpx_generator.py
+++ gpx_generator.py
@@ -7,6 +7,7 @@ import csv
 
 tmp_file = '/tmp/tmp.csv'
 
+
 def strip_iblue747(file_name):
     with open(file_name, 'r') as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
@@ -16,6 +17,7 @@ def strip_iblue747(file_name):
                 #stripped_row = row[0:14] + row[-1:]
                 stripped_row = row[0:2] + row[4:6] + row[7:14] + row[-1:]
                 csv_writer.writerow(stripped_row)
+
 
 def generate_gpx(csv_file):
     (file_name, file_ext) = os.path.splitext(csv_file)
@@ -28,7 +30,8 @@ def generate_gpx(csv_file):
     print(command_string)
     os.system(command_string)
 
-def main():
+
+if __name__ == '__main__':
     args = sys.argv[1:]
     assert len(args) >= 1
     arg = args[0]
@@ -37,8 +40,4 @@ def main():
     elif os.path.isdir(arg):
         for csv_file in glob.glob(arg+'/*.csv'):
             generate_gpx(csv_file)
-    
-
-if __name__ == '__main__':
-    main()
 
--- gpx_merger.py
+++ gpx_merger.py
@@ -4,6 +4,7 @@ import os
 import sys
 import glob
 
+
 def merge_gpx(directory_name, out_file):
     parameter_str = ''
     for gpx_file in glob.glob(directory_name+'/*.gpx'):
@@ -17,13 +18,12 @@ def merge_gpx(directory_name, out_file):
     print(command_string)
     os.system(command_string)
 
-def main():
+
+if __name__ == '__main__':
     args = sys.argv[1:]
     assert len(args) >= 1
     (dir_name, out_file) = args[0:2]
     if os.path.isdir(dir_name):
         merge_gpx(dir_name, out_file)
 
-if __name__ == '__main__':
-    main()
 
